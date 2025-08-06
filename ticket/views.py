a6f80ea012d82d746f5adfc06918c6cb09320a1d
Aayush Sapkota <sapkotaaayush708@gmail.com>
2025-08-06 21:04:27 +0545

    Initial Commit

---
+++ ticket/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import *
+
+# Create your views here.
+
